595f35bb668bb8974adc7f420d08e8efe35d965e
SabiKov <b00063874@student.itb.ie>
2016-10-30 01:01:00 +0100

    added four basic test cases

---
+++ Junit/src/test/java/com/in28minutes/junit/helper/StringHelperTest.java
@@ -0,0 +1,40 @@
+package com.in28minutes.junit.helper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringHelperTest {
+	
+	StringHelper helper = new StringHelper();
+
+	@Test
+	public void testTruncateAInFirst2Positions_AInFirst2Positions() {
+		//AACD => CD, ACD => CD, CDEF => CDEF, CDAA => CDAA
+
+		assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+		assertEquals("CDEF", helper.truncateAInFirst2Positions("CDEF"));
+		//excepted, actual
+	}
+	
+	@Test
+	public void testTruncateAInFirst2Positions_AInFirstPosition() {
+		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+		assertEquals("CDAA", helper.truncateAInFirst2Positions("CDAA"));
+	}
+	//expected:<ABC[]> but was:<ABC[D]>
+	
+	//ABCD => false, ABAB => true, AB => true, A => false,
+    @Test
+	public void testAreFirstAndLastTwoCharactersTheSame_BasicNegativeScenario() {
+    	assertFalse( 
+    			helper.areFirstAndLastTwoCharactersTheSame("ABCD"));
+    }
+    @Test
+	public void testAreFirstAndLastTwoCharactersTheSame_BasicPositiveScenario() {
+    	assertTrue( 
+    			helper.areFirstAndLastTwoCharactersTheSame("ABAB"));
+    }
+	
+}
